a8de03078aa4034da3a86cea6ea780dcfcfacb1c
Gugan M K <137408504+mkgugan22@users.noreply.github.com>
2024-02-13 13:52:22 +0530

    Update Transaction.js

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -11,12 +11,12 @@ const Transaction = () => {
     <div>
       Transaction
       <div>
-        <h3>Description<input type='text' placeholder='Enter Text' value={text} onChange={e=> setText(e.target.value)}></input></h3>
+        <h3>Description<input type='text' placeholder='Enter Text' value={text} onChange={e=> setText(e.target.value)} required></input></h3>
       </div>
       <div>
         <h3>amount<br/> [positive - income , negative - expense]
         <br></br>
-        <input type='number' placeholder='Enter Amount' value={transAmount} onChange={e=> setTransAmount(e.target.value)}></input></h3>
+        <input type='number' placeholder='Enter Amount' value={transAmount} onChange={e=> setTransAmount(e.target.value)} required></input></h3>
       </div>
       <div>
         <button onClick={handleSubmit}>Click</button>
